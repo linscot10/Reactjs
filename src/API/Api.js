b209ec80bbb49b4e4ace36ca1a8020ab18ca3df1
linscot10 <linscot10@gmail.com>
2025-01-13 23:52:17 +0300

    fetch

--- src/API/Api.js
+++ src/API/Api.js
@@ -1,9 +1,30 @@
 import React from 'react'
+import { useEffect } from 'react'
+import { useState } from 'react'
 
 const Api = () => {
-  return (
-    <div>Api</div>
-  )
+
+    const [data, setData] = useState([])
+    const [loading, setLoading] = useState('')
+    const [error, setError] = useState('')
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+                if (!response.ok)
+                    throw new Error("Network Response was not okay")
+            } catch (error) {
+
+            }
+        }
+    })
+    return (
+        <div>
+
+
+        </div>
+    )
 }
 
 export default Api
